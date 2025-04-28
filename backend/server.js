760e99c69ebe068e73da4253e1a8b9e3300a9023
vnsasi <116248503+vnsasi@users.noreply.github.com>
2025-04-28 16:29:52 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Configuration CORS
 const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:3000"
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:5002"
 };
 
 app.use(cors(corsOptions));
